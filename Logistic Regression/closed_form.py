18c354822887bd70b4a066410597271d1736d9d9
Bhanu Prasanna <63473951+bhanuprasanna2001@users.noreply.github.com>
2025-12-07 09:53:19 +0100

    Enhance comments on closed-form solution for logistic regression
    Added explanation for the absence of closed-form solution in logistic regression.

--- Logistic Regression/closed_form.py
+++ Logistic Regression/closed_form.py
@@ -1,2 +1,6 @@
-# There is no closed form solution for Logistic regression. 
+# There is no closed-form solution for Logistic regression. 
 # The MLE doesn't exist like for that exists for Linear Regression.
+
+# Why doesn't a closed-form solution exist? Unlike Linear regression, which has
+# this linear solution. The logistic regression makes use of this sigmoid function
+# which makes it non-linear, and can't be directly derived in the closed form solution.
